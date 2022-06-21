007798edb7fbcfde6552ab5a0476ac6dbb488cfc
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-06-21 17:01:31 +0700

    Template engine, static files vs scss and Bootrap 4

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')// dowload express library
+const path = require('path')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars');
+const app = express()
+const port = 3000
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+// route
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
