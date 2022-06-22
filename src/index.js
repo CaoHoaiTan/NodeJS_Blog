920f87f11078287f300dba3420595d50c4b1bc89
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-06-22 16:40:23 +0700

    Routes and Controller

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
+//require routes/index.js
+const routes = require('./routes');
+
+
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//handle form data of method post html
+app.use(express.urlencoded({
+  extended: true
+}));
+//handle send data xmlhttp, fetch, axios,...
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', engine({
@@ -18,14 +29,11 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-// route
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//Route init
+routes(app);
+
+
 // 127.0.0.1 - localhost
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
