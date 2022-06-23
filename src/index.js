c08a1ab692a47700be9432ca78c00ebb2a62be3d
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-06-23 10:11:22 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -36,12 +36,12 @@ app.engine(
   }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //Route init
 routes(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
