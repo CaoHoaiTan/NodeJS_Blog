b59f5774a20cb73942664053b0b181efc1c087fe
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-06-23 10:08:38 +0700

    Create Model and Connect to Mongo DB Update Route

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,21 @@
-
+const Course = require('../models/Course');
 class SiteController {
-    // [GET] /home
-    index(req, res) {
-        res.render('home');
-    }
+  // [GET] /home
+  index(req, res) {
+    Course.find({}, function (err, Course) {
+      if (!err) {
+        res.json(Course);
+        return;
+      }
+      res.status(400).json({ error: 'message' });
+    });
+    // res.render('home');
+  }
 
-    // [GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+  // [GET] /search
+  search(req, res) {
+    res.render('search');
+  }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-const express = require('express')// dowload express library
-const path = require('path')
-const morgan = require('morgan')
+const express = require('express'); // dowload express library
+const path = require('path');
+const morgan = require('morgan');
 const { engine } = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 //require routes/index.js
 const routes = require('./routes');
 
+// require db
+const db = require('./config/db/index');
+// connect to DB
+db.connect();
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 //handle form data of method post html
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 //handle send data xmlhttp, fetch, axios,...
 app.use(express.json());
 
@@ -23,17 +29,19 @@ app.use(express.json());
 // app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 //Route init
 routes(app);
 
-
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
