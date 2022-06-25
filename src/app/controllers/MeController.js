52c44d70d4d8775bd7000c04735525560c96542b
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-06-25 19:44:59 +0700

    Sort Middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,7 +3,14 @@ const { multipleMongooseToObject } = require('../../util/mongooes');
 class MeController {
   // [GET] /me//stored/courses
   storedCourses(req, res, next) {
-    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+    let courseQuery = Course.find({});
+    if (req.query.hasOwnProperty('_sort')) {
+      courseQuery = courseQuery.sort({
+        [req.query.column]: req.query.type,
+      });
+    }
+
+    Promise.all([courseQuery, Course.countDocumentsDeleted()])
       .then(([courses, deletedCount]) => {
         res.render('me/storedCourses', {
           courses: multipleMongooseToObject(courses),
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 //require routes/index.js
 const routes = require('./routes');
 
@@ -33,6 +35,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// custom middlewares
+app.use(SortMiddleware);
+
 // Template engine
 app.engine(
   'hbs',
@@ -40,6 +45,26 @@ app.engine(
     extname: '.hbs',
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default';
+        const icons = {
+          default: 'oi oi-elevator',
+          asc: 'oi oi-sort-ascending',
+          desc: 'oi oi-sort-descending',
+        };
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc',
+        };
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `  <a href="?_sort&column=${field}&type=${type}">
+        <span class="${icon}"></span>
+        </a> `;
+      },
     },
   }),
 );
